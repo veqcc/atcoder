af9a6bb57d8b24b4fcab04f9fcad23cddf694e5c
神戸隆太 <kambe3141@gmail.com>
2018-11-24 17:51:02 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -133,3 +133,16 @@ void bellman_ford() {
         }
     }
 }
+
+
+// Warshall-Floyd
+void warshall_floyd() {
+    int dist[n][n];
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+}
