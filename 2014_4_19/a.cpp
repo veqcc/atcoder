14f4c1061e18ca0559e4bde1b63ef831202acde9
神戸隆太 <kambe3141@gmail.com>
2018-11-30 15:11:47 +0900

    add 2014/4/19

---
+++ 2014_4_19/a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+typedef long long ll;
+typedef unsigned int uint;
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    cout << n-1 << "\n";
+    return 0;
+}
