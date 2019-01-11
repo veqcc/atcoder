1c17cef9ccd16c2923e5868dc98c70ca40d92fe3
神戸隆太 <kambe3141@gmail.com>
2019-01-11 11:35:53 +0900

    template topological sort

---
+++ templates/topological_sort.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+typedef long long ll;
+typedef unsigned int uint;
+using namespace std;
+
+struct TopologicalSort {
+    int n;
+    vector <set<int>> G;
+    vector<bool> used;
+    vector<int> indeg, p;
+
+    TopologicalSort() {}
+    TopologicalSort(int sz) : n(sz), G(n), used(n), indeg(n), p(0) {}
+
+    void add_edge(int s, int t) {
+        G[s].insert(t);
+    }
+
+    void bfs(int s) {
+        queue<int> q;
+        q.push(s);
+        used[s] = 1;
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            p.push_back(v);
+            for (int u:G[v]) {
+                indeg[u]--;
+                if (indeg[u] == 0 && !used[u]) {
+                    used[u] = 1;
+                    q.push(u);
+                }
+            }
+        }
+    }
+
+    vector<int> build() {
+        fill(used.begin(), used.end(), 0);
+        fill(indeg.begin(), indeg.end(), 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int v:G[i]) {
+                indeg[v]++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (indeg[i] == 0 && !used[i]) {
+                bfs(i);
+            }
+        }
+
+        return p;
+    }
+};
+
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M;
+    cin >> N >> M;
+
+    TopologicalSort ts(N);
+    for (int i = 0; i < M; i++) {
+        int s, t;
+        cin >> s >> t;
+        s--; t--;
+        ts.add_edge(s, t);
+    }
+
+    auto p = ts.build();
+
+    for (int node : p) {
+        cout << node << "->";
+    }
+
+    return 0;
+}
