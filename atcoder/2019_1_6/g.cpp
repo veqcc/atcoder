510a20ad3dd9c8030115bcd7ad47746f4f7cbd0b
神戸隆太 <kambe3141@gmail.com>
2019-01-11 11:29:52 +0900

    add 2019/1/6

--- atcoder/2019_1_6/g.cpp
+++ atcoder/2019_1_6/g.cpp
@@ -12,38 +12,91 @@ typedef long long ll;
 typedef unsigned int uint;
 using namespace std;
 
-vector <int> G[100005];
-int dp[100005];
+struct TopologicalSort {
+    int n;
+    vector <set<int>> G;
+    vector<bool> used;
+    vector<int> indeg, p;
 
-void dfs(int x, int y) {
-    if (dp[x] < y) {
-        dp[x] = y;
-        for (int i = 0; i < G[x].size(); i++) {
-            dfs(G[x][i], y+1);
+    TopologicalSort() {}
+    TopologicalSort(int sz) : n(sz), G(n), used(n), indeg(n), p(0) {}
+
+    void add_edge(int s, int t) {
+        G[s].insert(t);
+    }
+
+    void bfs(int s) {
+        queue<int> q;
+        q.push(s);
+        used[s] = 1;
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            p.push_back(v);
+            for (int u:G[v]) {
+                indeg[u]--;
+                if (indeg[u] == 0 && !used[u]) {
+                    used[u] = 1;
+                    q.push(u);
+                }
+            }
         }
     }
-}
+
+    vector<int> build() {
+        fill(used.begin(), used.end(), 0);
+        fill(indeg.begin(), indeg.end(), 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int v:G[i]) {
+                indeg[v]++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (indeg[i] == 0 && !used[i]) {
+                bfs(i);
+            }
+        }
+
+        return p;
+    }
+};
 
 int main() {
     cin.sync_with_stdio(false);
     cin.tie(0);
 
-    int n, m;
-    cin >> n >> m;
-    bool start[n];
-    fill(start, start+n, true);
-    for (int i = 0; i < m; i++) {
-        int x, y;
-        cin >> x >> y;
-        x--; y--;
-        G[x].push_back(y);
-        start[y] = false;
+    int N, M;
+    cin >> N >> M;
+    vector<int> table[N];
+
+    TopologicalSort ts(N);
+    for (int i = 0; i < M; i++) {
+        int s, t;
+        cin >> s >> t;
+        s--; t--;
+        ts.add_edge(s, t);
+        table[s].push_back(t);
     }
 
-    for (int i = 0; i < n; i++) {
-        if (start[i]) dfs(i, 1);
+    auto p = ts.build();
+
+    int dp[N];
+    fill(dp, dp + N, 0);
+
+    for (int node : p) {
+        for (int next : table[node]) {
+            dp[next] = max(dp[next], dp[node] + 1);
+        }
     }
 
-    cout << *max_element(dp, dp+100005) - 1 << "\n";
+    int ret = -1;
+    for (int i = 0; i < N; i++) {
+        ret = max(ret, dp[i]);
+    }
+
+    cout << ret << endl;
+
     return 0;
 }
