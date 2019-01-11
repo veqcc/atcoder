fa530f2922c26c83d7f011dde8ba5d2d3f67d18a
神戸隆太 <kambe3141@gmail.com>
2019-01-11 10:52:40 +0900

    add 2019/1/6

--- atcoder/2019_1_6/f.cpp
+++ atcoder/2019_1_6/f.cpp
@@ -16,23 +16,35 @@ int main() {
     string s, t;
     cin >> s >> t;
     int sl = s.length(), tl = t.length();
-    string dp[sl+1][tl+1];
-    fill(dp[0], dp[sl]+tl+1, "");
 
-    for (int i = 1; i <= sl; i++) {
-        for (int j = 1; j <= tl; j++) {
-            if (s[i-1] == t[j-1]) {
-                dp[i][j] = dp[i-1][j-1] + s[i-1];
+    int dp[sl+1][tl+1];
+    fill(dp[0], dp[sl+1], 0);
+
+    for (int i = 0; i < sl; i++) {
+        for (int j = 0; j < tl; j++) {
+            if (s[i] == t[j]) {
+                dp[i+1][j+1] = dp[i][j] + 1;
             } else {
-                if (dp[i-1][j].length() > dp[i][j-1].length()) {
-                    dp[i][j] = dp[i-1][j];
-                } else {
-                    dp[i][j] = dp[i][j-1];
-                }
+                dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j]);
             }
         }
     }
 
-    cout << dp[sl][tl] << "\n";
+    string ans = "";
+    int i = sl, j = tl;
+    while (dp[i][j] > 0) {
+        if (s[i-1] == t[j-1]) {
+            i--, j--;
+            ans += s[i];
+        } else if (dp[i-1][j] > dp[i][j-1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
+    cout << ans << "\n";
+
     return 0;
 }
