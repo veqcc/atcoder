ed48b8f95b48c0439390c594f401461aa7a794c7
神戸隆太 <kambe3141@gmail.com>
2019-01-12 20:37:36 +0900

    add 2019/1/6

--- atcoder/2019_1_6/o.cpp
+++ atcoder/2019_1_6/o.cpp
@@ -12,13 +12,38 @@ typedef long long ll;
 typedef unsigned int uint;
 using namespace std;
 
+const int mod = 1000000007;
+
 int main() {
     cin.sync_with_stdio(false);
     cin.tie(0);
 
     int n;
     cin >> n;
+    bool a[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
 
-    cout << n << "\n";
+    int dp[1 << n];
+    fill(dp, dp + (1 << n), 0);
+    dp[0] = 1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (1 << n) - 1; j++) {
+            if (i != __builtin_popcount(j)) continue;
+
+            for (int k = 0; k < n; k++) {
+                if (!(j & (1 << k)) && a[i][k]) {
+                    dp[j | (1 << k)] += dp[j];
+                    dp[j | (1 << k)] %= mod;
+                }
+            }
+        }
+    }
+
+    cout << dp[(1 << n) - 1] << "\n";
     return 0;
 }
