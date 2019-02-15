d90daf832d75d2c2125f23c5e59b6984b40e8d84
神戸隆太 <kambe3141@gmail.com>
2019-02-15 16:57:45 +0900

    yuki 786

---
+++ yuki/no786.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+typedef long long ll;
+typedef unsigned int uint;
+using namespace std;
+
+int n;
+ll dp[55];
+
+int main() {
+    cin >> n;
+
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    cout << dp[n] << "\n";
+    return 0;
+}
