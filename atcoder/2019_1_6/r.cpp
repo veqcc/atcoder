3cc75a950d7483c1d69da816b920b574f9cec5e2
神戸隆太 <kambe3141@gmail.com>
2019-02-19 17:08:31 +0900

    2019/1/6 r

---
+++ atcoder/2019_1_6/r.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+typedef long long ll;
+typedef unsigned int uint;
+using namespace std;
+
+const ll MOD = 1000000007LL;
+ll mod(ll a, ll m) { return (a % m + m) % m; }
+
+template<class T> struct Matrix {
+    vector <vector<T>> val;
+    Matrix(int n = 1, int m = 1) { val.clear(); val.resize(n, vector<T>(m)); }
+    Matrix(int n, int m, T x) { val.clear(); val.resize(n, vector<T>(m, x)); }
+    void init(int n, int m, T x = 0) { val.clear(); val.resize(n, vector<T>(m, x)); }
+    void resize(int n, int m, T x = 0) { val.resize(n); for (int i = 0; i < n; ++i) val[i].resize(m, x); }
+    int size() { return val.size(); }
+    inline vector <T> &operator[](int i) { return val[i]; }
+    friend ostream &operator<<(ostream &s, Matrix<T> M) { s << endl; for (int i = 0; i < M.val.size(); ++i) s << M[i] << endl; return s; }
+};
+
+template<class T> Matrix<T> operator * (Matrix<T> A, Matrix<T> B) {
+    Matrix<T> R(A.size(), B[0].size());
+    for (int i = 0; i < A.size(); ++i)
+        for (int j = 0; j < B[0].size(); ++j)
+            for (int k = 0; k < B.size(); ++k)
+                R[i][j] = mod(R[i][j] + A[i][k] * B[k][j], MOD);
+    return R;
+}
+
+template<class T> vector<T> operator * (Matrix<T> A, vector<T> B) {
+    vector <T> v(A.size());
+    for (int i = 0; i < A.size(); ++i)
+        for (int k = 0; k < B.size(); ++k)
+            v[i] = (v[i] + A[i][k] * B[k]) % MOD;
+    return v;
+}
+
+template<class T> Matrix<T> pow(Matrix<T> A, ll n) {
+    Matrix<T> R(A.size(), A.size());
+    for (int i = 0; i < A.size(); ++i) R[i][i] = 1;
+    while (n > 0) { if (n & 1) R = R * A; A = A * A; n >>= 1; }
+    return R;
+}
+
+int main() {
+    int n; ll k;
+    cin >> n >> k;
+    Matrix <ll> M(n, n, 0);
+    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) cin >> M[i][j];
+    vector <ll> vec(n, 1);
+    Matrix <ll> P = pow(M, k);
+    vector <ll> V = P * vec;
+    ll ans = 0LL;
+    for (int i = 0; i < n; i++) ans += V[i];
+    cout << ans % MOD << "\n";
+    return 0;
+}
