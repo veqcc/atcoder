433e2a9651aa6533732192e8f64b64647a0c02cc
神戸隆太 <kambe3141@gmail.com>
2019-02-28 17:25:36 +0900

    AOJ RMQ verify

---
+++ aoj/dsl/rmq.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+typedef long long ll;
+using namespace std;
+
+const int INF = 2147483647;
+int N, Q;
+
+struct SegmentTree {
+private:
+    int n;
+    vector <int> node;
+
+public:
+    SegmentTree(vector <int> v) {
+        int sz = v.size();
+        n = 1;
+        while (n < sz) n *= 2;
+        node.resize(2*n-1, INF);
+        for (int i = 0; i < sz; i++) node[i+n-1] = v[i];
+        for (int i = n-2; i >= 0; i--) node[i] = min(node[2*i+1], node[2*i+2]);
+    }
+
+    void update(int x, int val) {
+        x += n - 1;
+        node[x] = val;
+        while (x > 0) {
+            x = (x - 1) / 2;
+            node[x] = min(node[2*x+1], node[2*x+2]);
+        }
+    }
+
+    int getmin(int a, int b, int k=0, int l=0, int r=-1) {
+        if (r < 0) r = n;
+        if (r <= a || b <= l) return INF;
+        if (a <= l && r <= b) return node[k];
+        int vl = getmin(a, b, 2*k+1, l, (l+r)/2);
+        int vr = getmin(a, b, 2*k+2, (l+r)/2, r);
+        return min(vl, vr);
+    }
+};
+
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> Q;
+    SegmentTree seg(vector<int>(N, INF));
+    for (int i = 0; i < Q; i++) {
+        int c, x, y;
+        cin >> c >> x >> y;
+        if (c == 0) {
+            seg.update(x, y);
+        } else {
+            cout << seg.getmin(x, y+1) << "\n";
+        }
+    }
+
+    return 0;
+}
