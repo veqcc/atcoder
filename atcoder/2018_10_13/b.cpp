859d4443859877505d3997bccc8b2445fe59b2a2
神戸隆太 <kambe3141@gmail.com>
2019-03-02 23:59:37 +0900

    2018/10/13 b

--- atcoder/2018_10_13/b.cpp
+++ atcoder/2018_10_13/b.cpp
@@ -1,24 +1,54 @@
-#include<bits/stdc++.h>
-#include <math.h>
-#define rep(i,x,y) for (int i=(x);i<=(y);i++)
-#define ll long long
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+typedef long long ll;
 using namespace std;
-const ll N=1e5+5,mod=1e9+7;
-ll n,w,ans,inv[N],fac;
-ll sqr(int x){return x*x%mod;}
-ll pow(int a,int n){return n?sqr(pow(a,n/2))*(n&1?a:1)%mod:1;}
 
-int main(){
-    cin >> n;
-    inv[1] = 1;
-    rep (i,2,n) inv[i] = pow(i, mod-2);
-    rep (i,1,n) inv[i] = (inv[i] + inv[i-1]) % mod;
-    fac=1;
-    rep (i,1,n) {
-        cin >> w;
-        ans = (ans+(inv[i]+inv[n-i+1]-1) % mod * w % mod) % mod;
-        fac = fac * i % mod;
+const ll MOD = 1000000007LL;
+
+ll pow_mod(ll num, ll pow) {
+    ll prod = 1;
+    num %= MOD;
+    while (pow > 0) {
+        if (pow & 1) prod = prod * num % MOD;
+        num = num * num % MOD;
+        pow >>= 1;
     }
-    cout << ans * fac % mod << endl;
+    return prod;
+}
+
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+
+    ll n, a;
+    cin >> n;
+
+    ll n_pow = 1LL;
+    for (ll i = 1; i <= n; i++) (n_pow *= i) %= MOD;
+
+    ll sm[n];
+    sm[0] = 0;
+    for (ll i = 1; i < n; i++) {
+        sm[i] = n_pow * pow_mod(i+1, MOD-2) % MOD;
+        (sm[i] += sm[i-1]) %= MOD;
+    }
+
+    ll ans = 0LL;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        (ans += n_pow * a % MOD) %= MOD;
+        (ans += (sm[i] + sm[n-i-1]) % MOD * a % MOD) %= MOD;
+    }
+
+    cout << ans << "\n";
     return 0;
 }
