bf1f94ef8b59458eeaf94ad198bbf6cf5a277752
神戸隆太 <kambe3141@gmail.com>
2019-03-04 00:21:10 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -30,14 +30,14 @@ ll gcd(ll a, ll b) {
     if (b == 0) return a;
     return gcd(b, a % b);
 }
+
 ll lcm(ll a, ll b) {
     ll g = gcd(a, b);
     return a / g * b;
 }
 
 // numのpow乗をmodで計算する
-const ll mod = 1000000007LL;
-ll pow_mod(ll num, ll po) {
+ll pow_mod(ll num, ll pow, ll mod) {
     ll prod = 1;
     num %= mod;
     while (pow > 0) {
@@ -49,19 +49,20 @@ ll pow_mod(ll num, ll po) {
 }
 
 // Combination (n fixed)
-ll M = 1000000007;
+ll MOD = 1000000007;
 ll inv[100005], fac[100005], fiv[100005];
-ll combination(ll k) {
-    return fac[n] * fiv[k] % M * fiv[n-k] % M;
+
+ll comb(ll x, ll y) {
+    return fac[x] * fiv[y] % MOD * fiv[x-y] % MOD;
 }
+
 int main2() {
-    inv[1]=fac[1]=fiv[1]=inv[0]=fac[0]=fiv[0]=1;
+    inv[1] = fac[1] = fiv[1] = inv[0] = fac[0] = fiv[0] = 1;
     for (ll i = 2; i <= n; i++) {
-        fac[i] = fac[i-1] * i % M; // n!
-        inv[i] = inv[M%i] * (M-M/i) % M; // n^-1
-        fiv[i] = fiv[i-1] * inv[i] % M; // (n!)^-1
+        fac[i] = fac[i - 1] * i % MOD; // n!
+        inv[i] = inv[MOD % i] * (MOD - MOD / i) % M; // n^-1
+        fiv[i] = fiv[i - 1] * inv[i] % MOD; // (n!)^-1
     }
-    ll nCk = combination(k);
 }
 
 
@@ -70,9 +71,7 @@ bool sortbysec(const pair<int,int> &a, const pair<int,int> &b) {
     return (a.second < b.second);
 }
 int main3() {
-    for (int i = 0; i < n; i++) {
-        vec.push_back(make_pair(i, i+1));
-    }
+    for (int i = 0; i < n; i++) vec.push_back(make_pair(i, i+1));
     // sort by first element asc
     sort(vec.begin(), vec.end());
     // sort desc ?
