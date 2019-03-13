fa53072b0cc10425689a211e34f056f3c4686380
神戸隆太 <kambe3141@gmail.com>
2019-03-13 16:02:16 +0900

    2017/5/20 f

---
+++ atcoder/2017_5_20/f.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <random>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+typedef long long ll;
+using namespace std;
+
+const int INF = 1 << 30;
+
+struct Edge {
+    int to; int cap; int rev;
+    Edge(int t, int c, int r) : to(t), cap(c), rev(r) {}
+};
+
+struct Flow {
+    vector <vector<Edge>> G;
+    vector<bool> used;
+    Flow(int n) : G(n), used(n, false) {}
+    void add_edge(int from, int to, int cap) {
+        G[from].push_back(Edge(to, cap, G[to].size()));
+        G[to].push_back(Edge(from, 0, G[from].size() - 1));
+    }
+    int dfs(int v, int t, int f) {
+        if (v == t) return f;
+        used[v] = true;
+        for (int i = 0; i < G[v].size(); i++) {
+            Edge &e = G[v][i];
+            if (!used[e.to] && e.cap > 0) {
+                int d = dfs(e.to, t, min(f, e.cap));
+                if (d > 0) {
+                    e.cap -= d;
+                    G[e.to][e.rev].cap += d;
+                    return d;
+                }
+            }
+        }
+        return 0;
+    }
+    int max_flow(int s, int t) {
+        int flow = 0;
+        while (1) {
+            fill(used.begin(), used.end(), false);
+            int f = dfs(s, t, INF);
+            if (f == 0) return flow;
+            flow += f;
+        }
+    }
+};
+
+int main() {
+    int H, W;
+    cin >> H >> W;
+
+    Flow fl(H+W+2);
+    for (int i = 0; i < H; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < W; j++) {
+            if (s[j] == 'S') {
+                fl.add_edge(H+W, i, INF);
+                fl.add_edge(H+W, H+j, INF);
+            } else if (s[j] == 'T') {
+                fl.add_edge(i, H+W+1, INF);
+                fl.add_edge(H+j, H+W+1, INF);
+            } else if (s[j] == 'o') {
+                fl.add_edge(i, H+j, 1);
+                fl.add_edge(H+j, i, 1);
+            }
+        }
+    }
+
+    int ret = fl.max_flow(H+W, H+W+1);
+    if (ret >= INF) ret = -1;
+
+    cout << ret << "\n";
+    return 0;
+}
