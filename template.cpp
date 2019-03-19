ae9c0f1ac3813e1d9b3e6ccab280e727915b4d86
神戸隆太 <kambe3141@gmail.com>
2019-03-19 23:29:55 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <random>
+#include <bitset>
 #include <queue>
 #include <cmath>
 #include <stack>
@@ -12,6 +13,7 @@
 #include <map>
 typedef long long ll;
 using namespace std;
+const ll MOD = 1000000007LL;
 
 int main() {
     cin.sync_with_stdio(false);
@@ -47,23 +49,6 @@ ll pow_mod(ll num, ll pow, ll mod) {
         pow >>= 1;
     }
     return prod;
-}
-
-// Combination (n fixed)
-ll MOD = 1000000007;
-ll inv[100005], fac[100005], fiv[100005];
-
-ll comb(ll x, ll y) {
-    return fac[x] * fiv[y] % MOD * fiv[x-y] % MOD;
-}
-
-int main2() {
-    inv[1] = fac[1] = fiv[1] = inv[0] = fac[0] = fiv[0] = 1;
-    for (ll i = 2; i <= n; i++) {
-        fac[i] = fac[i - 1] * i % MOD; // n!
-        inv[i] = inv[MOD % i] * (MOD - MOD / i) % M; // n^-1
-        fiv[i] = fiv[i - 1] * inv[i] % MOD; // (n!)^-1
-    }
 }
 
 
