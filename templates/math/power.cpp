ffa90f46bef7f70e735aad0bb85044107bac7794
神戸隆太 <kambe3141@gmail.com>
2019-07-19 17:51:26 +0900

    template power

---
+++ templates/math/power.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef long long ll;
+const ll MOD = 1000000007LL;
+
+ll pow_mod(ll num, ll pow, ll mod) {
+    ll prod = 1;
+    num %= mod;
+    while (pow > 0) {
+        if (pow & 1) prod = prod * num % mod;
+        num = num * num % mod;
+        pow >>= 1;
+    }
+    return prod;
+}
+
+
+// verified
+//   https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_B
+void AOJ_NTL_1_B() {
+    ll m, n;
+    cin >> m >> n;
+    cout << pow_mod(m, n, MOD) << "\n";
+}
+
+int main() {
+    AOJ_NTL_1_B();
+    return 0;
+}
