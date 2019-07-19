86575b24ea672e15e7ac631cc191c62f20ad5218
神戸隆太 <kambe3141@gmail.com>
2019-07-19 17:47:37 +0900

    update template prime

--- templates/math/prime.cpp
+++ templates/math/prime.cpp
@@ -1,16 +1,7 @@
 #include <algorithm>
 #include <iostream>
-#include <iomanip>
-#include <cstring>
-#include <string>
 #include <vector>
-#include <queue>
-#include <cmath>
-#include <stack>
-#include <set>
 #include <map>
-typedef long long ll;
-typedef unsigned int uint;
 using namespace std;
 
 // O(√n) で素数判定
@@ -18,7 +9,6 @@ bool is_prime(int n) {
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) return false;
     }
-
     return n != 1;
 }
 
@@ -28,12 +18,9 @@ vector<int> divisor(int n) {
     for (int i = 1; i * i <= n; i++) {
         if (n % i == 0) {
             ret.push_back(i);
-            if (i != n / i) {
-                ret.push_back(n / i);
-            }
+            if (i != n / i) ret.push_back(n / i);
         }
     }
-
     return ret;
 }
 
@@ -46,30 +33,28 @@ map<int, int> prime_factor(int n) {
             n /= i;
         }
     }
-
     if (n != 1) ret[n] = 1;
     return ret;
 }
 
-int main() {
-    cin.sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
 
+// verified
+//   https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_A
+void AOJ_NTL_1_A() {
     int n;
     cin >> n;
+    cout << n << ":";
 
-    vector <int> vec = divisor(n);
-    for (int i = 0; i < vec.size(); i++) {
-        cout << vec[i] << " ";
+    map<int, int> mp = prime_factor(n);
+    for (auto itr = mp.begin(); itr != mp.end(); itr++) {
+        for (int i = 0; i < itr->second; i++) {
+            cout << " " << itr->first;
+        }
     }
     cout << "\n";
+}
 
-    map<int, int> :: iterator itr;
-    map<int, int> m = prime_factor(n);
-    for (itr = m.begin(); itr != m.end(); itr++) {
-        cout << itr->first << " : " << itr->second << "\n";
-    }
-
+int main() {
+    AOJ_NTL_1_A();
     return 0;
 }
