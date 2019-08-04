6a4a10fc9b4d657a78e68a669e0dfd1db25642bb
神戸隆太 <kambe3141@gmail.com>
2019-08-04 17:14:01 +0900

    2019/1/6 m

--- atcoder/2019_1_6/m.cpp
+++ atcoder/2019_1_6/m.cpp
@@ -1,42 +1,42 @@
+#include <functional>
 #include <algorithm>
 #include <iostream>
+#include <iomanip>
 #include <cstring>
 #include <string>
 #include <vector>
+#include <random>
+#include <bitset>
 #include <queue>
 #include <cmath>
 #include <stack>
 #include <set>
 #include <map>
 typedef long long ll;
-typedef unsigned int uint;
 using namespace std;
-
-ll mod = 1000000007LL;
+const ll MOD = 1000000007LL;
 
 int main() {
     cin.sync_with_stdio(false);
     cin.tie(0);
+    cout.tie(0);
 
     int n, k;
     cin >> n >> k;
-    int a[n];
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
 
-    ll dp[n+1][k+1];
-    fill(dp[0], dp[n]+k+1, 0LL);
-    dp[0][0] = 1LL;
+    int a[n + 1];
+    for (int i = 1; i <= n; i++) cin >> a[i];
 
+    vector <vector<ll>> dp(n + 1, vector<ll>(k + 1, 0LL));
+    // dp[i][j]: i人にちょうどj個の飴を配る方法が何通りあるか
+
+    dp[0][0] = 1;
     for (int i = 1; i <= n; i++) {
         dp[i][0] = 1LL;
         for (int j = 1; j <= k; j++) {
-            if (j <= a[i-1]) {
-                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % mod;
-            } else {
-                dp[i][j] = ((dp[i][j-1] + dp[i-1][j]) % mod + mod - dp[i-1][j - a[i-1] - 1]) % mod;
-            }
+            dp[i][j] += dp[i][j - 1] + dp[i - 1][j];
+            if (j > a[i]) dp[i][j] -= dp[i - 1][j - a[i] - 1];
+            dp[i][j] = (dp[i][j] + MOD) % MOD;
         }
     }
 
