9df48aeed6d68c7ab35f1ccb374af77645ac694d
神戸隆太 <kambe3141@gmail.com>
2019-08-11 15:26:52 +0900

    update template Z Algorithm

--- templates/string/z_algorithm.cpp
+++ templates/string/z_algorithm.cpp
@@ -1,40 +1,88 @@
-#include <algorithm>
 #include <iostream>
-#include <iomanip>
-#include <cstring>
 #include <string>
 #include <vector>
-#include <queue>
-#include <cmath>
-#include <stack>
-#include <set>
-#include <map>
 typedef long long ll;
 using namespace std;
+const ll MOD = 1000000007LL;
 
-// S と S(i:) の共通prefixの長さの配列を返す。 O(|S|)
+// Z Algorithm
+//  最長共通prefix長列を返す
+//  計算量: O(|S|)
+
+// アルゴリズム
+//  k > r           -> それまで計算したzの結果は利用できず、ナイーブに計算する
+//  z[k-l] < r-k+1  -> S[0:r-l] == S[l:r] であり、kがすっぽり入っているので一発
+//  z[k-l] >= r-k+1 -> S[0:r-k+1] == S[k:r] であるので、追加分を計算する
+
 vector <int> ZAlgorithm(const string& S) {
-    int sz = S.size();
-    vector <int> ret(sz);
-    ret[0] = sz;
+    int N = S.size();
+    vector<int> z(N);
+    z[0] = -1;
 
-    int i = 1, j = 0;
-    while (i < sz) {
-        while (i+j < sz && S[j] == S[i+j]) j++;
-        ret[i] = j;
-        if (j == 0) { i++; continue; }
-        int k = 1;
-        while (i+k < sz && k+ret[k] < j) ret[i+k] = ret[k], k++;
-        i += k; j -= k;
+    int l = 0, r = 0;
+    for (int k = 1; k < N; k++) {
+        if (k > r) {
+            int i = 0;
+            while (k + i < N && S[i] == S[k + i]) i++;
+            l = k;
+            r = k + i - 1;
+            z[k] = i;
+        } else if (z[k - l] < r - k + 1) {
+            z[k] = z[k - l];
+        } else if (z[k - l] >= r - k + 1) {
+            int i = r - k + 1;
+            while (k + i < N && S[i] == S[k + i]) i++;
+            l = k;
+            r = k + i - 1;
+            z[k] = i;
+        }
     }
 
-    return ret;
+    return z;
+}
+
+
+// verified (TLE)
+//  https://yukicoder.me/problems/no/430
+void yuki430() {
+    string S, C;
+    int M, ans = 0;
+    cin >> S >> M;
+    while (M--) {
+        cin >> C;
+        vector <int> Z = ZAlgorithm(C + S);
+        for (int i = C.size(); i < Z.size(); i++) if (Z[i] >= C.size()) ans++;
+    }
+    cout << ans << "\n";
+}
+
+
+// verified
+//  https://yukicoder.me/problems/no/599
+vector <ll> dp(10005);
+vector <bool> used(10005, false);
+ll dfs(const string &S, int L, int R) {
+    if (R <= L) return 1;
+    if (used[L]) return dp[L];
+    string s = S.substr(L, R - L);
+    vector <int> z = ZAlgorithm(s);
+    ll ret = 1;
+    int l = 1, r = s.size() - 1;
+    while (l <= r) {
+        if (z[r] == l) ret = (ret + dfs(S, L + l, L + r)) % MOD;
+        l++; r--;
+    }
+    used[L] = true;
+    return dp[L] = ret;
+}
+void yuki599() {
+    string s;
+    cin >> s;
+    cout << dfs(s, 0, s.size()) << '\n';
 }
 
 int main() {
-    string s = "aaabcdabaacaaabd";
-    //          *210001021042100
-    auto v = ZAlgorithm(s);
-    for (int x : v) cout << x << " ";
+    // yuki430();
+    yuki599();
     return 0;
 }
