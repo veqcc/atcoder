81418685c69dfca803468217a90ef8aeab222355
神戸隆太 <kambe3141@gmail.com>
2019-08-28 13:42:49 +0900

    2016/7/30 a

---
+++ atcoder/2016_7_30/a.cpp
@@ -0,0 +1,33 @@
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <random>
+#include <bitset>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+typedef long long ll;
+using namespace std;
+const ll MOD = 1000000007LL;
+
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int ans = 0;
+    for (int i = 1; i <= 100; i++) {
+        if (i % 3 != 0 && i % 5 != 0) {
+            ans += i;
+        }
+    }
+
+    cout << ans << "\n";
+    return 0;
+}
