56b2024088561014292c970f6c64c9e2d8bc17d8
神戸隆太 <kambe3141@gmail.com>
2019-09-08 23:09:39 +0900

    utcoder 2019/9/8

---
+++ utcoder/2019_9_8/a.cpp
@@ -0,0 +1,55 @@
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <random>
+#include <bitset>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+typedef long long ll;
+using namespace std;
+const ll MOD = 1000000007LL;
+const int MAX = 1 << 30;
+
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    int mx = 0;
+    vector <int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        mx = max(mx, a[i]);
+    }
+
+
+    vector<vector<int>> dp(n + 1, vector<int>(m));
+    dp[0][0] = 1;
+
+    int ans = MAX;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (dp[i][j]) {
+                dp[i + 1][j] |= dp[i][j];
+                if (j + a[i] >= m) {
+                    ans = min(ans, j + a[i]);
+                } else {
+                    dp[i + 1][j + a[i]] = 1;
+                }
+            }
+        }
+    }
+
+    cout << (ans == MAX ? -1 : ans) << "\n";
+    return 0;
+}
---
+++ utcoder/2019_9_8/c.cpp
@@ -0,0 +1,51 @@
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <random>
+#include <bitset>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+typedef long long ll;
+using namespace std;
+const ll MOD = 1000000007LL;
+
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    int mx = 0, mx_idx = 1;
+    for (int i = 2; i <= n; i++) {
+        int dist;
+        cout << "? " << 1 << " " << i << endl;
+        cin >> dist;
+
+        if (mx < dist) {
+            mx = dist;
+            mx_idx = i;
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (i == mx_idx) continue;
+
+        int dist;
+        cout << "? " << mx_idx << " " << i << endl;
+        cin >> dist;
+
+        mx = max(mx, dist);
+    }
+
+    cout << "! " << mx << "\n";
+    return 0;
+}
