0c149721c79889d2ac9eb0ef25fc77518e9c7b63
神戸隆太 <kambe3141@gmail.com>
2019-09-09 20:12:12 +0900

    update template SegmentTree

--- templates/data_structure/SegmentTree.cpp
+++ templates/data_structure/SegmentTree.cpp
@@ -11,29 +11,23 @@ class SegmentTree {
     F f;
     T t;
     vector <T> dat;
-
 public:
-    SegmentTree() {};
     SegmentTree(F f, T t) : f(f), t(t) {}
-
     void init(int n_) {
         n = 1;
         while (n < n_) n <<= 1;
         dat.assign(n << 1, t);
     }
-
     void build(const vector <T> &v) {
         auto n_ = (int)v.size();
         init(n_);
         for (int i = 0; i < n_; i++) dat[n + i] = v[i];
         for (int i = n - 1; i > 0; i--) dat[i] = f(dat[(i << 1) | 0], dat[(i << 1) | 1]);
     }
-
     void set_val(int k, T x) {
         dat[k += n] = x;
         while (k >>= 1) dat[k] = f(dat[(k << 1) | 0], dat[(k << 1) | 1]);
     }
-
     T query(int a, int b) {
         T vl = t, vr = t;
         for (int l = a + n, r = b + n; l < r; l >>= 1, r >>= 1) {
