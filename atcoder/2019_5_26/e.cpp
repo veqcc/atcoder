ac97d954edaa826cc716c6b8219dbe233384f681
神戸隆太 <kambe3141@gmail.com>
2019-09-14 21:56:56 +0900

    2019/5/26 e

--- atcoder/2019_5_26/e.cpp
+++ atcoder/2019_5_26/e.cpp
@@ -15,102 +15,42 @@ typedef long long ll;
 using namespace std;
 const ll MOD = 1000000007LL;
 typedef pair <int, int> P;
-typedef pair <int, P> PP;
-
-const int INF = 2147483647;
-
-struct LazySegmentTree {
-private:
-    int n;
-    vector <int> node, lazy;
-    vector <bool> lazyFlag;
-
-public:
-    LazySegmentTree(vector <int> v) {
-        int sz = v.size();
-        n = 1;
-        while (n < sz) n *= 2;
-        node.resize(2*n-1);
-        lazy.resize(2*n-1, 0);
-        lazyFlag.resize(2*n-1, false);
-        for (int i = 0; i < sz; i++) node[i+n-1] = v[i];
-        for (int i = n-2; i >= 0; i--) node[i] = min(node[2*i+1], node[2*i+2]);
-    }
-
-    void lazyEvaluate(int k, int l, int r) {
-        if (lazyFlag[k]) {
-            node[k] = lazy[k];
-            if (r - l > 1) {
-                lazy[k*2+1] = lazy[2*k+2] = lazy[k];
-                lazyFlag[k*2+1] = lazyFlag[k*2+2] = true;
-            }
-            lazyFlag[k] = false;
-        }
-    }
-
-    void update(int a, int b, int val, int k=0, int l=0, int r=-1) {
-        if (r < 0) r = n;
-        lazyEvaluate(k, l, r);
-        if (b <= l || r <= a) return;
-        if (a <= l && r <= b) {
-            lazy[k] = val;
-            lazyFlag[k] = true;
-            lazyEvaluate(k, l, r);
-        } else {
-            update(a, b, val, 2*k+1, l, (l+r)/2);
-            update(a, b, val, 2*k+2, (l+r)/2, r);
-            node[k] = min(node[2*k+1], node[2*k+2]);
-        }
-    }
-
-    int find(int a, int b, int k=0, int l=0, int r=-1) {
-        if (r < 0) r = n;
-        lazyEvaluate(k, l, r);
-        if (r <= a || b <= l) return INF;
-        if (a <= l && r <= b) return node[k];
-        int vl = find(a, b, 2*k+1, l, (l+r)/2);
-        int vr = find(a, b, 2*k+2, (l+r)/2, r);
-        return min(vl, vr);
-    }
-};
-
 
 int main() {
     cin.sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
-    int n, Q;
-    cin >> n >> Q;
+    int n, q;
+    cin >> n >> q;
 
-    vector <PP> vec(n);
+    vector <P> vec(2 * n);
     for (int i = 0; i < n; i++) {
         int s, t, x;
         cin >> s >> t >> x;
-        vec[i] = PP(x, P(s, t));
+        vec[i] = P(s - x, x);
+        vec[n + i] = P(t - x, -x);
     }
 
-    sort(vec.begin(), vec.end(), greater<PP>());
+    sort(vec.begin(), vec.end());
 
-    vector<int> D(Q + 1, -INF);
-    for (int i = 1; i <= Q; i++) cin >> D[i];
+    int idx = 0;
+    set <int> st;
+    while (q--) {
+        int d;
+        cin >> d;
 
-    vector<int> v(Q + 1, -1);
-    LazySegmentTree seg(v);
+        while (idx < vec.size() && vec[idx].first <= d) {
+            int x = vec[idx++].second;
+            if (x < 0) st.erase(-x);
+            else st.insert(x);
+        }
 
-    for (int i = 0; i < n; i++) {
-        int l = vec[i].second.first - vec[i].first;
-        int r = vec[i].second.second - vec[i].first;
-
-        int idx1 = lower_bound(D.begin(), D.end(), l) - D.begin();
-        int idx2 = lower_bound(D.begin(), D.end(), r) - D.begin();
-
-        seg.update(idx1, idx2, vec[i].first);
-    }
-
-    for (int q = 1; q <= Q; q++) {
-        cout << seg.find(q, q + 1) << "\n";
+        if (st.size()) cout << *st.begin() << '\n';
+        else cout << -1 << '\n';
     }
 
     return 0;
+
+
 }
