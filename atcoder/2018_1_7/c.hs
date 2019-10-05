3dade08ed14e9e05a23c3b5ae2c1f9612180838f
神戸隆太 <kambe3141@gmail.com>
2019-10-05 23:19:59 +0900

    2018/1/7 c

--- atcoder/2018_1_7/c.hs
+++ atcoder/2018_1_7/c.hs
@@ -1,7 +1,7 @@
 main = do
     [n,y] <- map read . words <$> getLine
-    let ans = [ [a, b, c] | a <- [0..n],
-                            b <- [0..n-a],
-                            let c = n - a - b,
-                            10000 * a + 5000 * b + 1000 * c == y ] ++ [replicate 3 (-1)]
-    putStrLn .unwords . map show . head $ ans
+    let ans = [[a, b, c] | a <- [0 .. n],
+                           b <- [0 .. n-a],
+                           let c = n - a - b,
+                           10000 * a + 5000 * b + 1000 * c == y ]
+    putStrLn $ unwords $ map show $ if null ans then [-1, -1, -1] else head ans
