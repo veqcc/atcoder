8bfa5ceaa2be454469e08019e2e9f8fdfaac0f86
神戸隆太 <kambe3141@gmail.com>
2020-06-18 02:09:55 +0900

    update template bellman_ford

--- templates/graph/bellman_ford.cpp
+++ templates/graph/bellman_ford.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-typedef pair <int, int> P;
-const int INF = 1 << 30;
+typedef long long ll;
+typedef pair <int, ll> P;
+const ll INF = 1ll << 60;
 
-vector <int> bellman_ford(vector<vector<P>> &edge, int s) {
-    int V = edge.size();
-    vector <int> dist(V, INF);
+void bellman_ford(
+        vector<vector<P>> &edge,
+        vector<ll> &dist,
+        vector<bool> &loop,
+        int s) {
+    int n = edge.size();
+    dist.resize(n, INF);
     dist[s] = 0;
-    for (int i = 1; i < V; i++) {
-        for (int from = 0; from < V; from++) {
+    for (int i = 1; i < n; i++) {
+        for (int from = 0; from < n; from++) {
             if (dist[from] == INF) continue;
             for (P e : edge[from]) {
                 int to = e.first;
@@ -18,18 +23,20 @@ vector <int> bellman_ford(vector<vector<P>> &edge, int s) {
         }
     }
 
-    // 負の閉路があるならば空のvectorを返す
-    for (int from = 0; from < V; from++) {
-        if (dist[from] == INF) continue;
-        for (P &e : edge[from]) {
-            int to = e.first;
-            if (dist[to] > dist[from] + e.second) {
-                return vector <int>();
+    loop.resize(n, false);
+    for (int i = 0; i < n; i++) {
+        for (int from = 0; from < n; from++) {
+            if (dist[from] == INF) continue;
+            for (P &e : edge[from]) {
+                int to = e.first;
+                if (dist[to] > dist[from] + e.second) {
+                    dist[to] = dist[from] + e.second;
+                    loop[from] = true;
+                    loop[to] = true;
+                }
             }
         }
     }
-
-    return dist;
 }
 
 
@@ -38,31 +45,48 @@ vector <int> bellman_ford(vector<vector<P>> &edge, int s) {
 void AOJ_GRL_1_B() {
     int V, E, r;
     cin >> V >> E >> r;
-
     vector<vector<P>> edge(V);
     for (int i = 0; i < E; i++) {
         int s, t, d;
         cin >> s >> t >> d;
         edge[s].push_back({t, d});
     }
-
-    vector <int> dist = bellman_ford(edge, r);
-
-    if (dist.size() == 0) {
-        cout << "NEGATIVE CYCLE" << "\n";
-        return;
-    }
-
-    for (int d : dist) {
-        if (d == INF) {
-            cout << "INF\n";
-        } else {
-            cout << d << "\n";
+    vector<ll> dist;
+    vector<bool> loop;
+    bellman_ford(edge, dist, loop, r);
+    for (int i = 0; i < V; i++) {
+        if (loop[i]) {
+            cout << "NEGATIVE CYCLE" << "\n";
+            return;
         }
     }
+    for (ll d : dist) {
+        if (d == INF) cout << "INF\n";
+        else cout << d << "\n";
+    }
+}
+
+
+// verified
+//  https://atcoder.jp/contests/abc061/tasks/abc061_d
+void AtCoder_2017_05_13_D() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<P>> edge(n);
+    for (int i = 0; i < m; i++) {
+        int a, b; ll c;
+        cin >> a >> b >> c;
+        edge[--a].push_back({--b, -c});
+    }
+    vector<ll> dist;
+    vector<bool> loop;
+    bellman_ford(edge, dist, loop, 0);
+    if (loop[n - 1]) cout << "inf" << endl;
+    else cout << -dist[n - 1] << endl;
 }
 
 int main() {
     AOJ_GRL_1_B();
+    // AtCoder_2017_05_13_D();
     return 0;
 }
