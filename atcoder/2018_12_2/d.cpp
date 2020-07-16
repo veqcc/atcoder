1e17ab4da649043e0abde5d86dc22d4f95126a92
神戸隆太 <kambe3141@gmail.com>
2020-07-16 14:15:29 +0900

    2018/12/2 d

--- atcoder/2018_12_2/d.cpp
+++ atcoder/2018_12_2/d.cpp
@@ -1,49 +1,53 @@
-#include <iostream>
+#include <functional>
 #include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
 #include <string>
 #include <vector>
+#include <random>
+#include <bitset>
 #include <queue>
 #include <cmath>
+#include <stack>
+#include <set>
+#include <map>
 typedef long long ll;
-typedef unsigned int uint;
 using namespace std;
+const ll MOD = 1000000007LL;
+
+map<int, int> prime_factor(int n) {
+    map<int, int> ret;
+    for (int i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            ret[i]++;
+            n /= i;
+        }
+    }
+    if (n != 1) ret[n] = 1;
+    return ret;
+}
 
 int main() {
-    cin.sync_with_stdio(false);
-    cin.tie(0);
     int n;
     cin >> n;
 
-    int w[101];
-    fill(w, w+101, 0);
-    for (int i = 2; i <= n; i++) {
-        int j = 2;
-        int t = i;
-        while (t > 1) {
-            if (t % j == 0) {
-                w[j]++;
-                t = t / j;
-            } else {
-                j++;
-            }
-        }
+    map<int, int> mp;
+    for (int i = 1; i <= n; i++) {
+        auto f = prime_factor(i);
+        for (int i = 1; i <= n; i++) mp[i] += f[i];
     }
 
-    int c2=0, c4=0, c14=0, c24=0, c74=0;
-    for (int i = 2; i < 101; i++) {
-        if (w[i] >= 74) c74++;
-        if (w[i] >= 24) c24++;
-        if (w[i] >= 14) c14++;
-        if (w[i] >= 4)  c4++;
-        if (w[i] >= 2)  c2++;
-    }
+    map<int, int> count;
+    for (int i = 1; i <= n; i++) count[mp[i]]++;
+    for (int i = n; i; i--) count[i - 1] += count[i];
 
     int ans = 0;
-    ans += c74;
-    ans += c24 * (c2-1);
-    ans += c14 * (c4-1);
-    ans += c4 * (c4-1) * (c2-2) / 2;
+    ans += count[74];
+    ans += count[24] * (count[2] - 1);
+    ans += count[14] * (count[4] - 1);
+    ans += count[4] * (count[4] - 1) / 2 * (count[2] - 2);
 
-    cout << ans;
+    cout << ans << endl;
     return 0;
 }
