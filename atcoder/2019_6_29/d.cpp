45bd3e92b2cb551da37266d731a2f5f4484bba7e
神戸隆太 <kambe3141@gmail.com>
2020-07-17 16:59:55 +0900

    2019/6/29 d

--- atcoder/2019_6_29/d.cpp
+++ atcoder/2019_6_29/d.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <algorithm>
 #include <iostream>
 #include <iomanip>
@@ -13,55 +14,91 @@
 #include <map>
 typedef long long ll;
 using namespace std;
-
-const int MAX_N = 100000;
 const ll MOD = 1000000007LL;
 
-ll inv[MAX_N], fac[MAX_N], fiv[MAX_N];
-
-void COMinit() {
-    inv[1] = fac[1] = fiv[1] = inv[0] = fac[0] = fiv[0] = 1;
-    for (ll i = 2; i < MAX_N; i++) {
-        fac[i] = fac[i - 1] * i % MOD; // n!
-        inv[i] = inv[MOD % i] * (MOD - MOD / i) % MOD; // n^-1
-        fiv[i] = fiv[i - 1] * inv[i] % MOD; // (n!)^-1
+template <ll mod> class ModInt {
+    ll a;
+public:
+    constexpr ModInt(const ll a = 0) noexcept : a((a % mod + mod) % mod) {}
+    constexpr ll &value() noexcept { return a; }
+    constexpr ModInt operator + (const ModInt &rhs) const noexcept { return ModInt(*this) += rhs; }
+    constexpr ModInt operator - (const ModInt &rhs) const noexcept { return ModInt(*this) -= rhs; }
+    constexpr ModInt operator * (const ModInt &rhs) const noexcept { return ModInt(*this) *= rhs; }
+    constexpr ModInt operator / (const ModInt &rhs) const noexcept { return ModInt(*this) /= rhs; }
+    constexpr ModInt &operator += (const ModInt &rhs) noexcept {
+        a += rhs.a;
+        if (a >= mod) a -= mod;
+        return *this;
     }
-}
+    constexpr ModInt &operator -= (const ModInt &rhs) noexcept {
+        a += mod - rhs.a;
+        if (a >= mod) a -= mod;
+        return *this;
+    }
+    constexpr ModInt &operator *= (const ModInt &rhs) noexcept {
+        a = a * rhs.a % mod;
+        return *this;
+    }
+    constexpr ModInt pow(ll t) const noexcept {
+        if (t == 0) return 1;
+        auto ret = pow(t >> 1);
+        ret *= ret;
+        if (t & 1) ret *= *this;
+        return ret;
+    }
+    constexpr ModInt inv() const noexcept { return pow(mod - 2); }
+    constexpr ModInt operator /=(const ModInt &rhs) { return (*this) *= rhs.inv(); }
+    constexpr bool operator == (const ModInt &rhs) const noexcept { return this->a == rhs.a; }
+    constexpr bool operator != (const ModInt &rhs) const noexcept { return this->a != rhs.a; }
+    friend constexpr ostream &operator << (ostream &os, const ModInt &rhs) noexcept { return os << rhs.a; }
+    friend constexpr istream &operator >> (istream &is, ModInt &rhs) {
+        is >> rhs.a;
+        return is;
+    }
+};
+using mint = ModInt<MOD>;
 
-ll com(ll n, ll k) {
-    if (n < k) return 0;
-    if (n < 0 || k < 0) return 0;
-    return fac[n] * fiv[k] % MOD * fiv[n-k] % MOD;
-}
+class Combination {
+public:
+    vector <mint> fac, inv, fiv;
+    Combination(int N) : fac(N + 1), inv(N + 1), fiv(N + 1) {
+        fac[0] = inv[0] = fiv[0] = fac[1] = inv[1] = fiv[1] = 1;
+        for (ll i = 2; i <= N; i++) {
+            fac[i] = fac[i - 1] * i; // n!
+            inv[i] = inv[MOD % i] * (MOD - MOD / i); // n^-1
+            fiv[i] = fiv[i - 1] * inv[i]; // (n!)^-1
+        }
+    }
+    mint C(ll n, ll k) {
+        if (k < 0 || n < k) return 0;
+        return fac[n] * fiv[k] * fiv[n - k];
+    }
+    mint P(ll n, ll k) {
+        if (k < 0 || n < k) return 0;
+        return fac[n] * fiv[n - k];
+    }
+    mint H(ll n, ll k) {
+        if (n == 0 && k == 0) return 1;
+        return C(n + k - 1, k - 1);
+    }
+};
 
 int main() {
-    cin.sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    ll n, k;
+    int n, k;
     cin >> n >> k;
+    Combination com(n);
 
-    ll red = n - k;
-
-    COMinit();
-
-    for (ll i = 1; i <= k; i++) {
-        if (red == 0) {
-            ll ans = 0;
-            if (i == 1) ans = 1;
-            cout << ans << "\n";
+    for (int i = 1; i <= k; i++) {
+        if (n == k && i == 1) {
+            cout << 1 << endl;
             continue;
         }
 
-        ll blue = com(k-1, i-1);
-
-        ll r = 0;
-        r = com(red - 1, i - 1) * 2 % MOD;
-        r = (r + com(red - 1, i - 2)) % MOD;
-        r = (r + com(red - 1, i)) % MOD;
-
-        cout << blue * r % MOD << "\n";
+        mint ans = 0;
+        ans += com.C(k - 1, i - 1) * com.C(n - k - 1, i - 1) * 2;
+        ans += com.C(k - 1, i - 1) * com.C(n - k - 1, i);
+        ans += com.C(k - 1, i - 1) * com.C(n - k - 1, i - 2);
+        cout << ans << endl;
     }
 
     return 0;
