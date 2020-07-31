736ea6525c53b0149b5ad974c50e67cfa27af5ea
神戸隆太 <kambe3141@gmail.com>
2020-07-31 13:38:47 +0900

    2020/3/21 a

--- atcoder/2020_3_21/a.cpp
+++ atcoder/2020_3_21/a.cpp
@@ -17,41 +17,34 @@ using namespace std;
 const ll MOD = 1000000007LL;
 
 int main() {
-    cin.sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
     int h, w;
     cin >> h >> w;
 
-    char s[h][w];
+    vector<string> s(h);
     for (int i = 0; i < h; i++) cin >> s[i];
 
-    int dp[h][w];
-    fill(dp[0], dp[h - 1] + w, 0);
+    vector<vector<int>> cnt(h, vector<int>(w, 1e9));
+    cnt[0][0] = s[0][0] == '#' ? 1 : 0;
 
-    if (s[0][0] == '#') dp[0][0] = 1;
-
-    for (int j = 1; j < w; j++) {
-        dp[0][j] = dp[0][j - 1];
-        if (s[0][j] != s[0][j - 1]) dp[0][j]++;
-    }
-
-    for (int i = 1; i < h; i++) {
-        dp[i][0] = dp[i - 1][0];
-        if (s[i][0] != s[i - 1][0]) dp[i][0]++;
-
-        for (int j = 1; j < w; j++) {
-            int tmp1 = dp[i - 1][j];
-            if (s[i][j] != s[i - 1][j]) tmp1++;
-
-            int tmp2 = dp[i][j - 1];
-            if (s[i][j] != s[i][j - 1]) tmp2++;
-
-            dp[i][j] = min(tmp1, tmp2);
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            if (i) {
+                if (s[i][j] == s[i - 1][j]) {
+                    cnt[i][j] = min(cnt[i][j], cnt[i - 1][j]);
+                } else {
+                    cnt[i][j] = min(cnt[i][j], cnt[i - 1][j] + 1);
+                }
+            }
+            if (j) {
+                if (s[i][j] == s[i][j - 1]) {
+                    cnt[i][j] = min(cnt[i][j], cnt[i][j - 1]);
+                } else {
+                    cnt[i][j] = min(cnt[i][j], cnt[i][j - 1] + 1);
+                }
+            }
         }
     }
 
-    cout << (dp[h - 1][w - 1] + 1) / 2 << "\n";
+    cout << (cnt[h - 1][w - 1] + 1) / 2 << endl;
     return 0;
 }
