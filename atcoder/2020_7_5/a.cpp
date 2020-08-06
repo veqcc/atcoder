b597211d8f151629da2a6a24dc0558f4e3255aa0
神戸隆太 <kambe3141@gmail.com>
2020-08-06 18:44:44 +0900

    2020/7/5 a b

---
+++ atcoder/2020_7_5/a.cpp
@@ -0,0 +1,25 @@
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <random>
+#include <bitset>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+typedef long long ll;
+using namespace std;
+const ll MOD = 1000000007LL;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << (1000 - n % 1000) % 1000 << endl;
+    return 0;
+}
---
+++ atcoder/2020_7_5/b.cpp
@@ -0,0 +1,36 @@
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <random>
+#include <bitset>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+typedef long long ll;
+using namespace std;
+const ll MOD = 1000000007LL;
+
+int main() {
+    int n;
+    cin >> n;
+
+    map<string, int> mp;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        mp[s]++;
+    }
+
+    cout << "AC x " << mp["AC"] << endl;
+    cout << "WA x " << mp["WA"] << endl;
+    cout << "TLE x " << mp["TLE"] << endl;
+    cout << "RE x " << mp["RE"] << endl;
+
+    return 0;
+}
